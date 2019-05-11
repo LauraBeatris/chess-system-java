85715709ed9e2c03598dac8abd9952eba00d8b59
Laura Beatris <laurabeatriserafim@gmail.com>
2019-05-11 13:23:38 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
+
 
 public class Program {
 
 	public static void main(String[] args) {
-		Position p = new Position(3, 5);
+		Board b = new Board(8, 8);
 		
-		System.out.println(p);
+		
+		
+		
+		
 	}
 
 }
